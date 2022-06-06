e73cdff425675524d78fe5f8c0a1f59a70421df0
ElPoderosoRafa <61497855+ElPoderosoRafa@users.noreply.github.com>
2022-06-06 15:12:38 -0300

    Delete trabalhoIA-importar esse arquivo directory

--- trabalhoIA-importar esse arquivo/core/src/br/uniruy/flappybirdia/entidades/Interface.java
+++
@@ -1,9 +0,0 @@
-package br.uniruy.flappybirdia.entidades;
-
-import com.badlogic.gdx.graphics.g2d.Batch;
-
-public interface Interface {
-    void dispose();
-    void draw(Batch batch);
-    void update(float deltaTime);
-}
